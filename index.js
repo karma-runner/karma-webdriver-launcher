e43a6974fd37e9e52b35dab07b3146d66dc00a27
Matthew Smith <mtscout6@gmail.com>
2014-10-30 14:21:33 -0600

    Added ability to define additional capabilities when initializing WebDriver connection

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
 var fs = require('fs');
 var wd = require('wd');
+var _ = {
+  assign: require('lodash.assign')
+};
 
 var WebDriverInstance = function (baseBrowserDecorator, args) {
   var config = args.config || {
     hostname: '127.0.0.1',
     port: 4444
   };
-  var spec = {
+  var desiredCapabilities = _.assign({
     browserName: args.browserName,
     version: args.version || '',
     platform: args.platform || 'ANY',
     tags: args.tags || [],
     name: args.testName || 'Karma test'
-  };
+  }, args.additionalCapabilities);
   var self = this;
 
   baseBrowserDecorator(this);
 
-  this.name = spec.browserName + ' via Remote WebDriver';
+  this.name = desiredCapabilities.browserName + ' via Remote WebDriver';
 
   this.on('kill', function(callback) {
     self.browser.quit(function() {
-      console.log('Killed ' + spec.name + '.');
+      console.log('Killed ' + desiredCapabilities.name + '.');
       callback();
     });
   });
 
   this._start = function (url) {
     self.browser = wd.remote(config);
-    self.browser.init(spec, function () {
+    self.browser.init(desiredCapabilities, function () {
       self.browser.get(url);
     });
   };
